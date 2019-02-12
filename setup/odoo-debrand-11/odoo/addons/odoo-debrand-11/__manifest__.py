063651159fdeb3bf8ed0e26b7894c6b00c6efdf9
Hilar Ak <hilarak@gmail.com>
2019-02-12 11:21:44 +0400

    [ADD] [FIX]

---
+++ setup/odoo-debrand-11/odoo/addons/odoo-debrand-11/__manifest__.py
@@ -0,0 +1,51 @@
+##############################################################################
+#
+#    Odoo, Odoo Debranding
+#    Copyright (C) 2019 Hilar AK All Rights Reserved
+#    https://www.linkedin.com/in/hilar-ak/
+#    <hilarak@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': "Odoo Debranding",
+
+    'summary': """
+        Odoo Module for backend and frontend debranding.""",
+
+    'description': """
+        To debrand front-end and back-end pages by removing
+         odoo promotions, links, labels and other related
+         stuffs.
+    """,
+
+    'author': "Hilar AK",
+    'website': "https://www.linkedin.com/in/hilar-ak/",
+    'category': 'Tools',
+    'version': '12.0.1.0.2',
+    'depends': [
+        'base_setup',
+        'website',
+    ],
+    'data': [
+        # 'data/mailbot_data.xml',
+        'views/views.xml',
+    ],
+    'qweb': ['static/src/xml/base.xml'],
+    'images': ["static/description/banner.gif"],
+    'license': "AGPL-3",
+    'installable': True,
+    'application': True,
+}
