8788c370b2df7fd9fcec9c982cbea0a29a8ee650
Hilar Ak <hilarak@gmail.com>
2020-01-15 12:46:37 +0400

    Update __manifest__.py

--- odoo-debrand-11/__manifest__.py
+++ odoo-debrand-11/__manifest__.py
@@ -33,7 +33,7 @@
     'author': "Hilar AK",
     'website': "https://www.linkedin.com/in/hilar-ak/",
     'category': 'Tools',
-    'version': '11.0.4.0.1',
+    'version': '11.0.4.0.2',
     'depends': [
         'base',
         'website',
